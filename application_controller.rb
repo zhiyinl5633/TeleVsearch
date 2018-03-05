a96aa4d36261e140635c0c128d00d05d18d96719
xinyanc3694 <xinyanc3694@hstat.org>
2018-03-05 03:44:07 +0000

    get pic, summary,img,genre and language to print

--- application_controller.rb
+++ application_controller.rb
@@ -8,10 +8,16 @@ require_relative 'models/model.rb'
 class ApplicationController < Sinatra::Base
 
   get '/' do
+   
     erb :index
   end
   
   post '/result' do
+     @user_name = params[:show_name] 
+     @user_img=get_img(@user_name)
+     @user_summary=get_summary(@user_name)
+     @user_language=get_language(@user_name)
+     @user_genres=get_genres(@user_name)
     erb :result
   end
   
--- models/model.rb
+++ models/model.rb
@@ -7,10 +7,72 @@ url = 'http://api.tvmaze.com/singlesearch/shows?q=w'
 uri = URI(url)
 response = Net::HTTP.get(uri)
 result = JSON.parse(response)
+puts result["name"]
+puts result["image"]["original"]
+puts result["summary"]
+puts result["language"]
+puts result["genres"]
 
-
-class Show
-    def initialize(name) 
-        
+# class Show
+#     def initialize(name) 
+#         @name= name
+#         # @img
+#         # @summary
+#         # @language
+#         # @genre
+#     end
+    # def get_info(name)
+    #     url = 'http://api.tvmaze.com/singlesearch/shows?q=w'
+    #     uri = URI(url)
+    #     response = Net::HTTP.get(uri)
+    #     result = JSON.parse(response)
+    #     result["name"]
+    #     result["image"]["original"]
+    #     result["summary"]
+    #     result["language"]
+    #     result["genres"]
+    # end
+    def get_name(name)
+        url = 'http://api.tvmaze.com/singlesearch/shows?q=w'
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        result = JSON.parse(response)
+        result["name"]
+    
     end
-end
+    def get_img(name)
+        url = 'http://api.tvmaze.com/singlesearch/shows?q=w'
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        result = JSON.parse(response)
+        result["image"]["original"]
+    end
+    def get_summary(name)
+        url = 'http://api.tvmaze.com/singlesearch/shows?q=w'
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        result = JSON.parse(response)
+        result["summary"]
+    end
+    def get_language(name)
+        url = 'http://api.tvmaze.com/singlesearch/shows?q=w'
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        result = JSON.parse(response)
+        result["language"]
+    end
+     def get_genres(name)
+        url = 'http://api.tvmaze.com/singlesearch/shows?q=w'
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        result = JSON.parse(response)
+        result["genres"].join(', ')
+    end
+# end
+
+# get_info("w")
+get_name("w")
+get_img("w")
+get_summary("w")
+get_language("w")
+get_genres("w")
